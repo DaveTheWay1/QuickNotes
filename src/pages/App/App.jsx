af09ea6bb2668e9e99306240e14b0c7c399f7267
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-05-14 14:07:52 -0700

    MERN-Stack Infrastructure

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,35 @@
+import '../../App.css';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import AuthPage from '../AuthPage/AuthPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
+import { useState } from 'react';
+// Add the following import
+import { Routes, Route } from 'react-router-dom';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import { getUser } from '../../utilities/users-service';
+
+export default function App() {
+  const [user, setUser ] = useState(getUser());
+  return (
+    <main className="App">
+      {
+        user ? 
+        <>
+          <NavBar user={user} setUser={setUser}/>
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <>
+          <AuthPage/>
+          <SignUpForm setUser={setUser}/>
+          <LoginForm setUser={setUser}/>
+        </>
+      }
+    </main>
+  );
+}
